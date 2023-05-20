549077f7097aacce14efe4f561df92621ee7c666
leew0nseok <zzang50aa@gmail.com>
2023-05-20 18:16:44 +0900

    .

--- src/main/java/com/example/demo/EnrolmentApplication.java
+++ src/main/java/com/example/demo/EnrolmentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class EnrolmentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(EnrolmentApplication.class, args);
     }
 
 }
--- src/test/java/com/example/demo/EnrolmentApplicationTests.java
+++ src/test/java/com/example/demo/EnrolmentApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class EnrolmentApplicationTests {
 
     @Test
     void contextLoads() {
