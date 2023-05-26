269cb123000b42660595734283839a235eabb118
yejineeeeee <dpwls0454@naver.com>
2023-05-26 18:23:01 +0900

    Registraion model구현

--- src/main/java/com/example/demo/domain/model/Registration.java
+++ src/main/java/com/example/demo/domain/model/Registration.java
@@ -1,4 +1,28 @@
 package com.example.demo.domain.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Registration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long registrationId;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    private LocalDateTime registrationDate;
 }
