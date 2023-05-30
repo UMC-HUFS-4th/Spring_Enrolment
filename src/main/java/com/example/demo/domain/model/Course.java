b76dffad8d2bebcb4aee1c13848a07a9f6201648
hidaeyeol <ydy9239@naver.com>
2023-05-30 20:55:55 +0900

    Course entity 수정

--- src/main/java/com/example/demo/domain/model/Course.java
+++ src/main/java/com/example/demo/domain/model/Course.java
@@ -3,6 +3,8 @@ package com.example.demo.domain.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,13 +16,13 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long courseId;
-
-    @ManyToOne // 다대일을 나타내기 위한 어노테이션
-    @JoinColumn(name = "student_id")  // student_id는 Student 테이블의 기본 키인 id와 연결됨 (외래키)
     private String courseTime;
     private String courseRoomNum;
     private String professorName;
     private int point;
     private int maxStudentNum;
     private int curStudentNum;
+
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private List<Registration> registrations = new ArrayList<>();
 }
