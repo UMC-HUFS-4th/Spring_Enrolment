967ba84b675d161de361e1d0715825a98e2c1cce
hidaeyeol <ydy9239@naver.com>
2023-05-30 20:31:20 +0900

    CourseService 추가

--- src/main/java/com/example/demo/domain/model/Course.java
+++ src/main/java/com/example/demo/domain/model/Course.java
@@ -17,13 +17,10 @@ public class Course {
 
     @ManyToOne // 다대일을 나타내기 위한 어노테이션
     @JoinColumn(name = "student_id")  // student_id는 Student 테이블의 기본 키인 id와 연결됨 (외래키)
-    private Student student;
     private String courseTime;
     private String courseRoomNum;
     private String professorName;
     private int point;
     private int maxStudentNum;
     private int curStudentNum;
-
-
 }
