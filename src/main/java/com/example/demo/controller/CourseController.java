9a08b6f5e6dcc2a4b7b6cfd99d3cea4168b2a295
leew0nseok <zzang50aa@gmail.com>
2023-06-02 19:40:44 +0900

    courseController url 수정, postman으로 강의 등록, 수정, 삭제, 조회 확인

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -23,19 +23,19 @@ public class CourseController {
     }
 
     // 특정 과목 조회
-    @GetMapping("/{courseId}")
+    @GetMapping("/get/{courseId}")
     public Course getCourseById(@PathVariable Long courseId) {
         return courseRepository.findById(courseId).orElse(null);
     }
 
     // 과목 추가
-    @PostMapping
+    @PostMapping("/open")
     public Course addCourse(@RequestBody Course course) {
         return courseRepository.save(course);
     }
 
     // 과목 수정
-    @PutMapping("/{courseId}")
+    @PutMapping("/update/{courseId}")
     public Course updateCourse(@PathVariable Long courseId, @RequestBody Course updatedCourse) {
         Course course = courseRepository.findById(courseId).orElse(null);
         if (course != null) {
@@ -51,7 +51,7 @@ public class CourseController {
     }
 
     // 과목 삭제
-    @DeleteMapping("/{courseId}")
+    @DeleteMapping("/delete/{courseId}")
     public void deleteCourse(@PathVariable Long courseId) {
         courseRepository.deleteById(courseId);
     }
